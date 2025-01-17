c15cbb24884c90bee4f855d123d0b89e3d5c2e9f
-S <npshunxiang@gmail.com>
2025-01-17 15:52:12 +0800

    first time using git for VS2022- trying out what commit does and what happens if 2 people commit at the same time  (findings it will have a conflict)

--- S10270399_PRG2Assignment/Flight.cs
+++ S10270399_PRG2Assignment/Flight.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10270399_PRG2Assignment
+{
+    abstract class Flight : IComparable<Flight>
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime Expectedtime { get; set; }
+        public string Status { get; set; }
+
+        public Flight() { }
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedtime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            Expectedtime = expectedtime;
+            Status = status;
+        }
+
+
+
+
+        public abstract double CalculateFees();
+
+        //public override string ToString()
+        //{
+        //    return "FlightNumber" + FlightNumber +
+        //        "Origin" + Origin +
+        //        "Destination" + Destination +
+        //        "ExpectedTime" + ExpectedTime +
+        //        "Status" + Status;
+        //}
+        public override string ToString()
+        {
+            return $"Flight {FlightNumber} from {Origin} to {Destination} at {Expectedtime:g} - Status: {Status}";
+        }
+
+
+        public int CompareTo(Flight other)
+        {
+            if (other == null) return 1;
+            return Expectedtime.CompareTo(other.Expectedtime);
+        }
+    }
+}
